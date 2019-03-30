1a893e15375a22a480b3d13ae8304161139c0d4b
CalumDarroch <calum_darroch@hotmail.com>
2019-03-30 13:03:47 +0000

    Exercise 8.7

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names and cohorts of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
+  months = ["january", "february", "march", "april", "may", "june", "july",
+  "august", "september", "october", "november", "december", "tbc"]
   # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  cohort = gets.chomp
+  # while the name and cohort are not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    if cohort.empty?
+      cohort = "TBC"
+    end
+    
+    while true do
+      if months.include? cohort.downcase
+        students << {name: name.to_sym, cohort: cohort.to_sym}
+        puts "Now we have #{students.count} students"
+        break
+      else
+        puts "Did you make a mistake when typing the cohort?"
+        cohort = gets.chomp
+      end
+    end
+    
     # get another name from the user
     name = gets.chomp
+    cohort = gets.chomp
   end
   # return the array of students
   students
@@ -24,7 +41,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
