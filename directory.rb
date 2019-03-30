8ba9e7a9dae35270d7e524b7b27b76e1aa55f9f3
CalumDarroch <calum_darroch@hotmail.com>
2019-03-30 14:35:09 +0000

    Exercise 8.9

--- directory.rb
+++ directory.rb
@@ -1,34 +1,21 @@
 def input_students
-  puts "Please enter the names and cohorts of the students"
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  months = ["january", "february", "march", "april", "may", "june", "july",
-  "august", "september", "october", "november", "december", "tbc"]
   # get the first name
   name = gets.chomp
-  cohort = gets.chomp
-  # while the name and cohort are not empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    if cohort.empty?
-      cohort = "TBC"
-    end
-    
-    while true do
-      if months.include? cohort.downcase
-        students << {name: name, cohort: cohort}
-        puts "Now we have #{students.count} students"
-        break
+    students << {name: name, cohort: :november}
+      if students.length == 1
+        puts "Now we have #{students.count} student"
       else
-        puts "Did you make a mistake when typing the cohort?"
-        cohort = gets.chomp
+        puts "Now we have #{students.count} students"
       end
-    end
-    
     # get another name from the user
     name = gets.chomp
-    cohort = gets.chomp
   end
   # return the array of students
   students
@@ -40,16 +27,9 @@ def print_header
 end
 
 def print(students)
-  while true do
-    puts "Which cohort would you like to print?"
-    print_cohort = gets.chomp.downcase
-    
-    filtered_students = students.select { |student| student[:cohort] == print_cohort}
-    
-    filtered_students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end  
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_footer(students)
